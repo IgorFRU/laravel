fb2b034e9eb0fda2814254612558896fc986dce3
Volkolak <igor.parketmir@gmail.com>
2018-11-15 16:03:51 +0300

    styles

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -13,36 +13,88 @@
 
     </head>
     <body>
-    <div class="fastmenu">
-       <ul>
-            <li>Паркетный Мир</li>
-            <li>все виды паркета по лучшим ценам</li>
-        </ul>
-        <ul>
-            <li>Симферополь</li>
-            <li>пр. Победы, 129/2</li>
-        </ul>
-        <ul>
-            <li>8(978) 816 01 66</li>
-            <li>8(978) 880 82 06</li>
-        </ul>
-        <ul>
-            <li class="fastmenu__work_today">Сегодня до 18:00</li>
-            <div>
-                <ul>
-                    <li>ПН-ПТ: 09:00 - 18:00</li>
-                    <li>СБ: 09:00 - 16:00</li>
-                </ul>
+    <div class="menu">
+        <div class="topmenu">
+            <div class="wrap">
+                <div class="topmenu__body">
+                    <div class="topmenu__left">
+                        <a href="#">О нас</a>
+                        <a href="#">Доставка</a>
+                        <a href="#">Оплата</a>
+                        <a href="#">Контакты</a>
+                        <a href="#">Статьи</a>
+                        <a href="#" class="topmenu__left__red">Акции</a>
+                    </div>
+                    <div class="topmenu__right">
+                        <a href="#">Вход</a>
+                        <a href="#">Регистрация</a>
+                    </div>
+                </div>
             </div>
-        </ul>
+        </div>
+        <div class="fastmenu">
+            <div class="wrap">
+                <div class="fastmenu__body">
+                   <ul>
+                        <li>Паркетный Мир</li>
+                        <li>все виды паркета по лучшим ценам</li>
+                    </ul>
+                    <ul>
+                        <li>Симферополь</li>
+                        <li>пр. Победы, 129/2</li>
+                    </ul>
+                    <ul>
+                        <li>8(978) 816 01 66</li>
+                        <li>8(978) 880 82 06</li>
+                    </ul>
+                    <ul>
+                        <li class="fastmenu__work_today">Сегодня до 18:00</li>
+                        <div>
+                            <ul>
+                                <li>ПН-ПТ: 09:00 - 18:00</li>
+                                <li>СБ: 09:00 - 16:00</li>
+                            </ul>
+                        </div>
+                    </ul>
+                </div>
+            </div>
+        </div>
     </div>
     <menu class="mainmenu">
-        <ul>
-            <li>Главная</li>
-            <li>Каталог товаров</li>
-            <li>Статьи</li>
-            <li>О нас</li>
-            <li>Контакты</li>
+        <ul class="mainmenu__ul">
+            <li class="mainmenu__li"><a href="#" class="mainmenu__a">Главная</a></li>
+            <li class="mainmenu__li"><a href="#" class="mainmenu__a">Полы</a>
+                <ul>
+                    <li><a href="#">Паркет штучный</a></li>
+                    <li><a href="#">Паркетная доска</a></li>
+                    <li><a href="#">Массивная доска</a></li>
+                    <li><a href="#">Ламинат</a></li>
+                    <li><a href="#">Пробковый паркет</a></li>
+                </ul>
+            </li>
+            <li class="mainmenu__li"><a href="#" class="mainmenu__a">Стены</a>
+                <ul>
+                    <li><a href="#">Пробковые стеновые панели</a></li>
+                    <li><a href="#">Ламинированные стеновые панели</a></li>
+                    <li><a href="#">Шпонированные стеновые панели</a></li>
+                </ul>
+            </li>
+            <li class="mainmenu__li"><a href="#" class="mainmenu__a">Химия</a>
+                <ul>
+                    <li><a href="#">Клей для паркета</a></li>
+                    <li><a href="#">Лак</a></li>
+                    <li><a href="#">Масла и воски</a></li>
+                    <li><a href="#">Клей для пробки</a></li>
+                </ul>
+            </li>
+            <li class="mainmenu__li"><a href="#" class="mainmenu__a">Погонаж</a></li>
+            <li class="mainmenu__li"><a href="#" class="mainmenu__a">Расходные материалы</a></li>
+            <li class="mainmenu__li"><a href="#" class="mainmenu__a">Услуги</a>
+                <ul>
+                    <li><a href="#">Работы</a></li>
+                    <li><a href="#">Аренда оборудования</a></li>
+                </ul>
+            </li>
         </ul>
     </menu>
 <!--
