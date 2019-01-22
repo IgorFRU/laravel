ce67a0c325fa1e4943861180de88738093367762
Volkolak <igor.parketmir@gmail.com>
2019-01-22 18:04:01 +0300

    edit admin

--- resources/views/admin/core.blade.php
+++ resources/views/admin/core.blade.php
@@ -2,46 +2,233 @@
 
 @section('content')
 
-   <h1>Admin</h1>
 
-   <div class="categories light_grey_box">
-      <h2 class="categories__title">Категории</h2>
-       <a href="#" class="category grey_box">
-           <div class="category__title">Название категории</div>
-<!--           <div class="category__detail">-->
-               <span class="category__detail_watches"><i class="fa fa-eye" aria-hidden="true"></i> 100</span>
-               <span class="category__detail_items"><i class="fa fa-th-large" aria-hidden="true"></i>25</span>
-<!--           </div>-->
-       </a>
-       <a href="#" class="category grey_box">
-           <div class="category__title">Название категории</div>
-<!--           <div class="category__detail">-->
-               <span class="category__detail_watches"><i class="fa fa-eye" aria-hidden="true"></i> 2565</span>
-               <span class="category__detail_items"><i class="fa fa-th-large" aria-hidden="true"></i>365</span>
-<!--           </div>-->
-       </a>
-       <a href="#" class="category grey_box">
-           <div class="category__title">Название категории</div>
-<!--           <div class="category__detail">-->
-               <span class="category__detail_watches"><i class="fa fa-eye" aria-hidden="true"></i>7</span>
-               <span class="category__detail_items"><i class="fa fa-th-large" aria-hidden="true"></i>2</span>
-<!--           </div>-->
-       </a>
-       <a href="#" class="category grey_box">
-           <div class="category__title">Название категории</div>
-<!--           <div class="category__detail">-->
-               <span class="category__detail_watches"><i class="fa fa-eye" aria-hidden="true"></i>15698</span>
-               <span class="category__detail_items"><i class="fa fa-th-large" aria-hidden="true"></i>1356</span>
-<!--           </div>-->
-       </a>
-       <a href="#" class="category grey_box">
-           <div class="category__title">Название категории</div>
-<!--           <div class="category__detail">-->
-               <span class="category__detail_watches"><i class="fa fa-eye" aria-hidden="true"></i> 356</span>
-               <span class="category__detail_items"><i class="fa fa-th-large" aria-hidden="true"></i>36</span>
-<!--           </div>-->
-       </a>
-   </div>
+<div class="categories light_grey_box">
+    <h2 class="categories__title">Категории</h2>
+    <a href="#" class="category grey_box">
+        <div class="category__title">Название категории</div>
+        <!--           <div class="category__detail">-->
+        <span class="category__detail_watches"><i class="fa fa-eye" aria-hidden="true"></i> 100</span>
+        <span class="category__detail_items"><i class="fa fa-th-large" aria-hidden="true"></i>25</span>
+        <!--           </div>-->
+    </a>
+    <a href="#" class="category grey_box">
+        <div class="category__title">Название категории</div>
+        <!--           <div class="category__detail">-->
+        <span class="category__detail_watches"><i class="fa fa-eye" aria-hidden="true"></i> 2565</span>
+        <span class="category__detail_items"><i class="fa fa-th-large" aria-hidden="true"></i>365</span>
+        <!--           </div>-->
+    </a>
+    <a href="#" class="category grey_box">
+        <div class="category__title">Название категории</div>
+        <!--           <div class="category__detail">-->
+        <span class="category__detail_watches"><i class="fa fa-eye" aria-hidden="true"></i>7</span>
+        <span class="category__detail_items"><i class="fa fa-th-large" aria-hidden="true"></i>2</span>
+        <!--           </div>-->
+    </a>
+    <a href="#" class="category grey_box">
+        <div class="category__title">Название категории</div>
+        <!--           <div class="category__detail">-->
+        <span class="category__detail_watches"><i class="fa fa-eye" aria-hidden="true"></i>15698</span>
+        <span class="category__detail_items"><i class="fa fa-th-large" aria-hidden="true"></i>1356</span>
+        <!--           </div>-->
+    </a>
+    <a href="#" class="category grey_box">
+        <div class="category__title">Название категории</div>
+        <!--           <div class="category__detail">-->
+        <span class="category__detail_watches"><i class="fa fa-eye" aria-hidden="true"></i> 356</span>
+        <span class="category__detail_items"><i class="fa fa-th-large" aria-hidden="true"></i>36</span>
+        <!--           </div>-->
+    </a>
+</div>
+
+<div class="categories light_grey_box">
+    <h2 class="categories__title">Подкатегории</h2>
+    
+    <a href="#" class="subcategory grey_box">
+        <div class="category__img">
+            <img src="/imgs/parquet_background_01.jpg" alt="">
+        </div>
+        <p>Родительская категория</p>
+            <div class="subcategory__info">
+            <div class="subcategory__title">Название подкатегории</div>
+
+            <span class="subcategory__detail_watches"><i class="fa fa-eye" aria-hidden="true"></i> 100</span>
+            <span class="subcategory__detail_items"><i class="fa fa-th-large" aria-hidden="true"></i>25</span>
+        </div>
+        
+    </a>
+    
+    <a href="#" class="subcategory grey_box">
+        <div class="category__img">
+            <img src="/imgs/parquet_background_01.jpg" alt="">
+        </div>
+        <p>Родительская категория</p>
+            <div class="subcategory__info">
+            <div class="subcategory__title">Название подкатегории</div>
+
+            <span class="subcategory__detail_watches"><i class="fa fa-eye" aria-hidden="true"></i> 100</span>
+            <span class="subcategory__detail_items"><i class="fa fa-th-large" aria-hidden="true"></i>25</span>
+        </div>
+        
+    </a>
+    
+    <a href="#" class="subcategory grey_box">
+        <div class="category__img">
+            <img src="/imgs/parquet_background_01.jpg" alt="">
+        </div>
+        <p>Родительская категория</p>
+            <div class="subcategory__info">
+            <div class="subcategory__title">Название подкатегории</div>
+
+            <span class="subcategory__detail_watches"><i class="fa fa-eye" aria-hidden="true"></i> 100</span>
+            <span class="subcategory__detail_items"><i class="fa fa-th-large" aria-hidden="true"></i>25</span>
+        </div>
+        
+    </a>
+    
+    <a href="#" class="subcategory grey_box">
+        <div class="category__img">
+            <img src="/imgs/parquet_background_01.jpg" alt="">
+        </div>
+        <p>Родительская категория</p>
+            <div class="subcategory__info">
+            <div class="subcategory__title">Название подкатегории</div>
+
+            <span class="subcategory__detail_watches"><i class="fa fa-eye" aria-hidden="true"></i> 100</span>
+            <span class="subcategory__detail_items"><i class="fa fa-th-large" aria-hidden="true"></i>25</span>
+        </div>
+        
+    </a>
+    
+    <a href="#" class="subcategory grey_box">
+        <div class="category__img">
+            <img src="/imgs/parquet_background_01.jpg" alt="">
+        </div>
+        <p>Родительская категория</p>
+            <div class="subcategory__info">
+            <div class="subcategory__title">Название подкатегории</div>
+
+            <span class="subcategory__detail_watches"><i class="fa fa-eye" aria-hidden="true"></i> 100</span>
+            <span class="subcategory__detail_items"><i class="fa fa-th-large" aria-hidden="true"></i>25</span>
+        </div>
+        
+    </a>
+    
+    <a href="#" class="subcategory grey_box">
+        <div class="category__img">
+            <img src="/imgs/parquet_background_01.jpg" alt="">
+        </div>
+        <p>Родительская категория</p>
+            <div class="subcategory__info">
+            <div class="subcategory__title">Название подкатегории</div>
+
+            <span class="subcategory__detail_watches"><i class="fa fa-eye" aria-hidden="true"></i> 100</span>
+            <span class="subcategory__detail_items"><i class="fa fa-th-large" aria-hidden="true"></i>25</span>
+        </div>
+        
+    </a>
+    
+    <a href="#" class="subcategory grey_box">
+        <div class="category__img">
+            <img src="/imgs/parquet_background_01.jpg" alt="">
+        </div>
+        <p>Родительская категория</p>
+            <div class="subcategory__info">
+            <div class="subcategory__title">Название подкатегории</div>
+
+            <span class="subcategory__detail_watches"><i class="fa fa-eye" aria-hidden="true"></i> 100</span>
+            <span class="subcategory__detail_items"><i class="fa fa-th-large" aria-hidden="true"></i>25</span>
+        </div>
+        
+    </a>
+    
+    <a href="#" class="subcategory grey_box">
+        <div class="category__img">
+            <img src="/imgs/parquet_background_01.jpg" alt="">
+        </div>
+        <p>Родительская категория</p>
+            <div class="subcategory__info">
+            <div class="subcategory__title">Название подкатегории</div>
+
+            <span class="subcategory__detail_watches"><i class="fa fa-eye" aria-hidden="true"></i> 100</span>
+            <span class="subcategory__detail_items"><i class="fa fa-th-large" aria-hidden="true"></i>25</span>
+        </div>
+        
+    </a>
+    
+    <a href="#" class="subcategory grey_box">
+        <div class="category__img">
+            <img src="/imgs/parquet_background_01.jpg" alt="">
+        </div>
+        <p>Родительская категория</p>
+            <div class="subcategory__info">
+            <div class="subcategory__title">Название подкатегории</div>
+
+            <span class="subcategory__detail_watches"><i class="fa fa-eye" aria-hidden="true"></i> 100</span>
+            <span class="subcategory__detail_items"><i class="fa fa-th-large" aria-hidden="true"></i>25</span>
+        </div>
+        
+    </a>
+    
+    <a href="#" class="subcategory grey_box">
+        <div class="category__img">
+            <img src="/imgs/parquet_background_01.jpg" alt="">
+        </div>
+        <p>Родительская категория</p>
+            <div class="subcategory__info">
+            <div class="subcategory__title">Название подкатегории</div>
+
+            <span class="subcategory__detail_watches"><i class="fa fa-eye" aria-hidden="true"></i> 100</span>
+            <span class="subcategory__detail_items"><i class="fa fa-th-large" aria-hidden="true"></i>25</span>
+        </div>
+        
+    </a>
+    
+    <a href="#" class="subcategory grey_box">
+        <div class="category__img">
+            <img src="/imgs/parquet_background_01.jpg" alt="">
+        </div>
+        <p>Родительская категория</p>
+            <div class="subcategory__info">
+            <div class="subcategory__title">Название подкатегории</div>
+
+            <span class="subcategory__detail_watches"><i class="fa fa-eye" aria-hidden="true"></i> 100</span>
+            <span class="subcategory__detail_items"><i class="fa fa-th-large" aria-hidden="true"></i>25</span>
+        </div>
+        
+    </a>
+    
+    <a href="#" class="subcategory grey_box">
+        <div class="category__img">
+            <img src="/imgs/parquet_background_01.jpg" alt="">
+        </div>
+        <p>Родительская категория</p>
+            <div class="subcategory__info">
+            <div class="subcategory__title">Название подкатегории</div>
+
+            <span class="subcategory__detail_watches"><i class="fa fa-eye" aria-hidden="true"></i> 100</span>
+            <span class="subcategory__detail_items"><i class="fa fa-th-large" aria-hidden="true"></i>25</span>
+        </div>
+        
+    </a>
+    
+    <a href="#" class="subcategory grey_box">
+        <div class="category__img">
+            <img src="/imgs/parquet_background_01.jpg" alt="">
+        </div>
+        <p>Родительская категория</p>
+            <div class="subcategory__info">
+            <div class="subcategory__title">Название подкатегории</div>
+
+            <span class="subcategory__detail_watches"><i class="fa fa-eye" aria-hidden="true"></i>99999</span>
+            <span class="subcategory__detail_items"><i class="fa fa-th-large" aria-hidden="true"></i>2500</span>
+        </div>
+        
+    </a>
+    
+    
+</div>
 
 
 @endsection
